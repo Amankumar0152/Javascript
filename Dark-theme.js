a44729a17b9681a84e02ba39b732b0fcedb51c86
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-09 22:03:48 +0530

    first commit

---
+++ Dark-theme.js
@@ -0,0 +1,12 @@
+// Dark theme configuration
+// This code sets the color based on the theme mode
+
+let mode = "dark";
+let color;
+
+if(mode === "dark"){
+    color = "black";
+}else{
+    color = "white"
+}
+console.log(color);
