4646fc4412b87e69daf47f90650f12fd204de0da
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-09 22:39:22 +0530

    ternary Ope

---
+++ conditional-sta/ternary-operator.js
@@ -0,0 +1,7 @@
+// This file demonstrates the use of the ternary operator in JavaScript.
+// The ternary operator is a shorthand for an if-else statement.
+
+let age = 22;
+
+let result = age >= 18 ? "adult" : "minor";
+console.log(result);
