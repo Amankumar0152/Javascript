c8ab1abd34bb5f16fd03601604a4a0910c521347
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-10 13:41:34 +0530

    more loops

---
+++ loops/do-while.js
@@ -0,0 +1,9 @@
+// This file contains a simple do-while loop that prints "Hi Shruti" 10 times.
+// It is a demonstration of how to use a do-while loop in JavaScript.
+// This code run at least once before checking the condition.
+let i =20;
+do{
+    console.log("Hi Shruti ");
+    i++;
+}while(i<=10);
+// This code will not execute as the condition is false, demonstrating the do-while loop's
---
+++ loops/while-loop.js
@@ -0,0 +1,8 @@
+// This file contains a simple while loop that prints "Hi Aman" 10 times.
+// It is a demonstration of how to use a while loop in JavaScript.
+
+let i =1;
+while(i<=10){
+    console.log("Hi Aman");
+    i++;
+}
