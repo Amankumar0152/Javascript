afa5a8025da4e939b50cf7b05850decb741a4bba
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-11 14:18:46 +0530

    array methods

---
+++ array/array-methods.js
@@ -0,0 +1,9 @@
+//
+
+let fruits = ["potato", "apple", "mango", "banana"];
+// fruits.push("chips", "lauda", "lahsun");
+
+console.log(fruits);
+let deleted = fruits.pop();
+console.log(fruits);
+console.log("Deleted Item: ",deleted)
