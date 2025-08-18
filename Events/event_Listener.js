2ad47132231620c4299c303e9d71311f3ef94ed6
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-18 23:09:42 +0530

    handler 3 removed

--- Events/event_Listener.js
+++ Events/event_Listener.js
@@ -8,14 +8,14 @@ btn1.addEventListener("click", () =>{
     console.log("btn1 click by handler 2")
 })
 
-btn1.addEventListener("click", () =>{
-    console.log("handler 3");
-})
+const handler3 = () =>{
+    console.log("handler 3 remove function");
+}
+
+btn1.addEventListener("click", handler3)
 
 btn1.addEventListener("click", () =>{
     console.log("Handler 4");
 })
 
-btn1.removeEventListener("click", () =>{
-    console.log("Event 3 handler removed");
-})
+btn1.removeEventListener("click", handler3);
