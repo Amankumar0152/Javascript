9f89e4e369862ed43c0fb81456bb73a9fdcd3a5d
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-18 23:19:01 +0530

    dark theme function EventListener

--- dark-theme/script.js
+++ dark-theme/script.js
@@ -1,17 +1,16 @@
-let modeBtn = document.querySelector("#mode");
+let modebtn = document.querySelector("#mode");
 let body = document.querySelector("body");
 let currMode = "light";
 
-modeBtn.addEventListener("click", () =>{
+modebtn.addEventListener("click", () =>{
     if(currMode === "light"){
         currMode = "dark";
         body.classList.add("dark");
-        body.classList.remove("white");
+        body.classList.remove("light");
     }else{
         currMode = "light";
-        body.classList.add("white");
+        body.classList.add("light");
         body.classList.remove("dark");
     }
-
     console.log(currMode);
 })
