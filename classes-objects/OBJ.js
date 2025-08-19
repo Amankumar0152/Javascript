e1f5ac2c9ad81c85b7453367e111d4c43dcafe12
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-19 17:20:38 +0530

    Prototype

--- classes-objects/OBJ.js
+++ classes-objects/OBJ.js
@@ -7,5 +7,20 @@ const employee = {
 const karanArjun ={
     salary: 50000,
 }
+const karanArjun2 ={
+    salary: 50000,
+}
+
+const karanArjun3 ={
+    salary: 50000,
+}
+
+const karanArjun4 ={
+    salary: 50000,
+}
+
 
 karanArjun.__proto__ = employee;
+karanArjun2.__proto__ = employee;
+karanArjun3.__proto__ = employee;
+karanArjun4.__proto__ = employee;
