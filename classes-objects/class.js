b948274f9af3729e02dcf74a40bf09c2a633e49a
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-21 16:39:17 +0530

    functions

--- classes-objects/class.js
+++ classes-objects/class.js
@@ -6,6 +6,12 @@ class Car{
     stop(){
         console.log("stop");
     }
+    setBrand(brand){
+        this.brandName = brand;
+    }
 }
 
 let dezire = new Car();
+dezire.setBrand("Dezire");
+let bmw = new Car();
+bmw.setBrand("BMW");
