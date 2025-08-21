b8171c59948222b440247b79417c0371124c9e1b
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-21 22:07:29 +0530

    inheritance

--- classes-objects/inheritance.js
+++ classes-objects/inheritance.js
@@ -1,25 +1,23 @@
 class person{
+    constructor(){
+        console.log("Enter parent Constructor");
+        this.species = "Home Sapiens";
+    }
     eat(){
-        console.log("Peoples eating");
-    }
-    sleep(){
-        console.log("Sleeping");
-    }
-    work(){
-        console.log("Eat 5 Star Do nothing");
+        console.log("eating");
     }
 }
 
 class Engineer extends person{
+    constructor(branch){
+        console.log("Enter child constructor");
+        super(); // to invoke parent class constructor
+        this.branch = branch;
+        console.log("Exit child constructor");
+    }
     work(){
         console.log("I need coffee After 5PM");
     }
 }
 
-class Doctor extends person{
-    work(){
-        console.log("Patients");
-    }
-}
-
-let objEng = new Engineer();
+let engobj = new Engineer("Software Eng");
