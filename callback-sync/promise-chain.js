3b1eb0c9c4f1d8d8430b2894c822e6a370c5c1c1
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-22 21:20:51 +0530

    promise chaining

--- callback-sync/promise-chain.js
+++ callback-sync/promise-chain.js
@@ -21,10 +21,9 @@ console.log("Fetching data1......")
 let p1 = asyncFunc();
 p1.then((res) =>{
     console.log(res);
-})
-
-console.log("Fetching data2......")
-let p2 = asyncFunc2();
-p1.then((res) =>{
-    console.log(res);
+    console.log("Fetching data2......");
+    let p2 = asyncFunc();
+    p2.then((res) =>{
+        console.log(res);
+    })
 })
