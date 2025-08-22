bad74c94974f4b176a511accac6ad4cf70c79065
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-22 10:13:10 +0530

    error handling try catch

--- Questions/Q10.js
+++ Questions/Q10.js
@@ -11,5 +11,18 @@ class User{
     }
 }
 
+class Admin extends User{
+    constructor(name, email){
+        super(name, email);
+    }
+    editData(){
+        data = "Data updated";
+    }
+}
+
 let student1 = new User("Aman", "Amanghostvilla@gmail.com");
 let student2 = new User("Shruti", "Shruticutevaranasi@gmail.com");
+
+let teacher1 = new User("teacher", "teacher@gmail.com");
+
+let admin = new Admin("admin", "admin-access@gmail.com");
---
+++ classes-objects/error-handling.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 10;
+console.log(a);
+console.log(b);
+console.log("A+ B", a+b);
+console.log("A+ B", a-b);
+
+console.log("A+ B", a*b);
+try{
+    console.log("A+ B", a/c);
+}catch(err){
+    console.log(err);
+}
+
+console.log("A+ B", a+b);
+console.log("A+ B", a+b);
