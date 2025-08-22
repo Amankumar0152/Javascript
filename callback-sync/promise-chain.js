70fdf180a4a359c1707f2fa9836a7b256572f8e6
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-22 21:17:03 +0530

    chaining

---
+++ callback-sync/promise-chain.js
@@ -0,0 +1,30 @@
+function asyncFunc(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            console.log("Some data");
+            resolve("success");
+        }, 4000);
+    })
+}
+
+function asyncFunc2(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            console.log("Some data2");
+            resolve("success");
+        }, 4000);
+    })
+}
+
+
+console.log("Fetching data1......")
+let p1 = asyncFunc();
+p1.then((res) =>{
+    console.log(res);
+})
+
+console.log("Fetching data2......")
+let p2 = asyncFunc2();
+p1.then((res) =>{
+    console.log(res);
+})
