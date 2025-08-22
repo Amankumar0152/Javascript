fb537c63d30e501d32700bc841d90f38c1199f30
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-22 18:17:17 +0530

    promise, then, catch function

--- callback-sync/promises.js
+++ callback-sync/promises.js
@@ -1,16 +1,28 @@
-// let promise = new Promise ((resolve, reject) =>{
-//     console.log("im a Promise");
-//     reject("some error occured")
-// })
-
-function getData(dataId, getNextData){
-    return new Promise((resolve, reject) =>{
-        setTimeout(() =>{
-            console.log("data", dataId);
-            resolve("success"); 
-            if(getNextData){
-                getNextData();
-            }
-        }, 5000);
-    })
+const getPromise = () =>{
+    return new Promise ((resolve, reject) =>{
+    console.log("This is Promose");
+    resolve("success");
+    reject("errors");
+})
 }
+
+let promise = getPromise();
+promise.then(() =>{
+    console.log("Promise fullfiled", res);
+})
+
+promise.catch(()=>{
+    console.log("Rejected", err);
+})
+
+// function getData(dataId, getNextData){
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() =>{
+//             console.log("data", dataId);
+//             resolve("success"); 
+//             if(getNextData){
+//                 getNextData();
+//             }
+//         }, 5000);
+//     })
+// }
