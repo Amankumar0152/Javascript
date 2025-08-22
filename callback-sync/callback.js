0e0feb91564800d0e22b4ab04a02ef73271222ce
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-22 17:17:46 +0530

    callback hell

---
+++ callback-sync/callback.js
@@ -0,0 +1,20 @@
+function getData(dataId, getNextData){
+    setTimeout(() =>{
+        console.log("Data: ", dataId);
+        if(getNextData){
+            getNextData();
+        }
+    },2000);
+}
+
+//callback hell
+getData(1, () =>{
+    console.log("getting data 2......");
+    getData(2, () =>{
+        console.log("getting data 3......");
+        getData(3, () =>{
+            console.log("getting data 4......");
+            getData(4);
+        });
+    });
+});
